c9ae6c9db537131e9cf2315c29af26b8462dbedf
Eldar <tmshulk@gmail.com>
2022-12-01 22:38:18 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        // origin: "https://task-seven-client.up.railway.app/",
+        origin: "https://tourmaline-valkyrie-1e7458.netlify.app/",
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("join_room", (data) => {
+        console.log(data);
+        socket.join(data);
+    });
+
+    socket.on("send_move", (data) => {
+        console.log(data);
+        socket.to(data.room).emit("receive_move", data);
+    });
+
+    socket.on("send_restart", (data) => {
+        console.log(data);
+        socket.to(data.room).emit("receive_restart", data);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log("SERVER IS RUNNING");
+});
